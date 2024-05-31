5849b28f43377464766d6ea18d3cfd2f493a6ee7
Xarderos <99949891+Xarderos@users.noreply.github.com>
2024-05-31 19:22:40 +0200

    a

---
+++ Assets/OpenMouth.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenMouth : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Animator top;
+    public Animator bot;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+   
+
+    }
+    public void Open()
+    {
+        top.SetBool("tremol", true);
+        top.SetBool("open", true);
+    }
+}
