30014a8a06d30e42c3fd3c4f11c3e0ceee0a3c83
Xarderos <99949891+Xarderos@users.noreply.github.com>
2024-06-04 22:55:10 +0200

    A

--- Assets/clock.cs
+++ Assets/clock.cs
@@ -1,8 +1,13 @@
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
 
 public class clock : MonoBehaviour
 {
+    public Image blackScreen;
+    public float fadeDuration = 1f;
     public TextMeshPro textMeshPro;
     public float countdownTime = 300f; //temps en segons
     private float currentTime;
@@ -18,11 +23,13 @@ public class clock : MonoBehaviour
         currentTime = countdownTime;
         UpdateText();
         startcd = false;
+        blackScreen.color = new Color(0, 0, 0, 0);
+
     }
 
     void Update()
     {
-        if (startcd == true && currentTime > 0)
+        if (startcd == true && currentTime >= 0)
         {
             currentTime -= Time.deltaTime;
             UpdateText();
@@ -37,16 +44,17 @@ public class clock : MonoBehaviour
 
     void UpdateText()
     {
-        int minutes = Mathf.FloorToInt(currentTime / 60);
-        int seconds = Mathf.FloorToInt(currentTime % 60);
-        textMeshPro.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+        if (currentTime > 0)
+        {
+            int minutes = Mathf.FloorToInt(currentTime / 60);
+            int seconds = Mathf.FloorToInt(currentTime % 60);
+            textMeshPro.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+        }
     }
 
     void OnCountdownEnd()
     {
-  
-        //TODO: S'acaba el timer
-
+        FadeToScene("Lose");
     }
     public void StartCd()
     {
@@ -59,4 +67,38 @@ public class clock : MonoBehaviour
         tictac.Stop();
 
     }
+    public void FadeToScene(string sceneName)
+    {
+        StartCoroutine(FadeOutIn(sceneName));
+    }
+
+    private IEnumerator FadeOutIn(string sceneName)
+    {
+        // Fade to black
+        yield return StartCoroutine(Fade(1f));
+
+        // Cambiar de escena
+        SceneManager.LoadScene(sceneName);
+
+        // Fade from black
+        yield return StartCoroutine(Fade(0f));
+    }
+
+    private IEnumerator Fade(float targetAlpha)
+    {
+        float startAlpha = blackScreen.color.a;
+        float elapsedTime = 0f;
+
+        while (elapsedTime < fadeDuration)
+        {
+            elapsedTime += Time.deltaTime;
+            float alpha = Mathf.Lerp(startAlpha, targetAlpha, elapsedTime / fadeDuration);
+            blackScreen.color = new Color(0, 0, 0, alpha);
+            yield return null;
+        }
+
+        // Asegurarse de que el Alpha sea exactamente el targetAlpha al final
+        blackScreen.color = new Color(0, 0, 0, targetAlpha);
+    }
+
 }
